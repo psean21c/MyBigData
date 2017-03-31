5cbad77c0e45afbfe4ca577f633f3125672e16a1
Simon Park <psean21c@gmail.com>
2017-03-31 14:22:06 -0400

    Update UserService.java

--- spring/boot/autowire/UserService.java
+++ spring/boot/autowire/UserService.java
@@ -1,13 +1,8 @@
 package com.acc.autowired.sample2;
 
-import javax.annotation.PostConstruct;
 
-import org.springframework.stereotype.Service;
-
-//@Service
 public class UserService {
 
-//	@PostConstruct
 	public void init() {
 		System.out.println("service from @service");
 	}
